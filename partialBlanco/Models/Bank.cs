bea3fbf97e13f2b45e4b206e3208597721e80760
Marcos <jkmarcosb@gmail.com>
2020-03-07 14:09:43 -0600

    Add project files.

---
+++ partialBlanco/Models/Bank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partialBlanco.Models
+{
+    public class Bank
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string NIT { get; set; }
+        public BankType Type { get; set; }
+    }
+}
---
+++ partialBlanco/Models/BankType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partialBlanco.Models
+{
+    public class BankType
+    {
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+
+        public State state { get; set; }
+    }
+}
